a0b71ff92f5b266dd2b2cbfde3fcbe33fff45fd2
Dayvson <dayvsonnlacerda360@gmail.com>
2026-01-19 20:03:01 -0300

    cancelado erro de scripts

--- backend/src/routes/progess.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const progressController = require('../controllers/progressController');
-const { authMiddleware } = require('../middleware/auth');
-
-router.post('/', authMiddleware, progressController.updateProgress);
-router.get('/roadmap/:roadmapId', authMiddleware, progressController.getUserProgress);
-
-module.exports = router;
